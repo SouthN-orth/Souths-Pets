b12ecc389e5c7a9457d0a25c50664cd8d12c5df8
kreezxil <kreezxil@gmail.com>
2018-10-06 09:49:13 -0500

    1.12.2-1.1.2
    Changed build.gradle and References.java and mcmod.info to handle dynamic version where only build.gradle needs to be updated to change a version number.

--- src/main/java/south/bison/util/Reference.java
+++ src/main/java/south/bison/util/Reference.java
@@ -2,11 +2,11 @@ package net.south.bison.util;
 
 public class Reference {
 
-    public static final String MODID = "fb";
+    public static final String MODID = "flyingbisons";
     public static final String NAME = "FlyingBisons";
-    public static final String VERSION = "1.0";
-    public static final String CLIENT = "net.south.bison.ClientProxy";
-    public static final String COMMON = "net.south.bison.CommonProxy";
+    public static final String VERSION = "@VERSION@";
+    public static final String CLIENT = "net.south.bison.proxy.ClientProxy";
+    public static final String COMMON = "net.south.bison.proxy.CommonProxy";
 
     public static final int ENTITY_AIRBISON = 120;
 
